c1c6c5ad1b508c2f8c8e6a9968b5806cfb3b4d61
rory-y <rory.elllaa@gmail.com>
2025-03-07 19:26:05 +0200

    lab_03/Student Functions megírva

--- labs/lab_03/Students/student_functions.c
+++ labs/lab_03/Students/student_functions.c
@@ -16,6 +16,23 @@ void readStudentDetails(Student_t *pStudent)
 void
 printStudentDetails (Student_t pStudent)
 {
-  printf("")
+  printf("%s\n", pStudent.name);
+  printf("%s\n", pStudent.neptunCode);
+  printf("%s\n", pStudent.birthPlace);
+  printf("%d/%d/%d\n", pStudent.dateOfBirth.year, pStudent.dateOfBirth.month,pStudent.dateOfBirth.day);
+  switch (pStudent.gender)
+    {
+    case '0':
+      printf("NON BINARY");
+      break;
+    case '1':
+      printf("MALE");
+      break;
+    case '2':
+      printf("FEMALE");
+      break;
+    default:
+    }
+  printf("%.2f\n", pStudent.examResult);
 }
 
